7356ec75da94e56ddf4a763463f4817f61878216
Daerosha <140925733+Daerosha@users.noreply.github.com>
2023-07-30 04:18:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+VANTA.CELLS({
+	el: "#antr",
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.00,
+	minWidth: 200.00,
+	scale: 1.00,
+	color1: 0x8c0031,
+	color2: 0xffed00,
+	size: 0.70
+  })
